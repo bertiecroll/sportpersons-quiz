3e58098b705d0c3a4a6e7701b997ca61b931dd82
Bertie Croll <bert.croll@gmail.com>
2017-02-03 15:30:48 +0000

    displayCards method added to Quiz class

--- client/specs/Quiz.test.js
+++ client/specs/Quiz.test.js
@@ -21,4 +21,23 @@ describe('Quiz', function() {
   it('should start with a user', function() {
     expect(testQuiz.user).toEqual({name: "Player 1"})
   })
+
+  it('should start at round one', function() {
+    expect(testQuiz.round).toEqual(1)
+  })
+
+  it('should start with no cards on show', function() {
+    expect(testQuiz.cardsOnShow.length).toEqual(0)
+  })
+
+  it('should be able to show cards for round 1', function() {
+    testQuiz.displayCards()
+    expect(testQuiz.cardsOnShow.length).toEqual(2)
+  })
+
+  it('should increase amount of cards on show by one each round', function() {
+    testQuiz.round = 2
+    testQuiz.displayCards()
+    expect(testQuiz.cardsOnShow.length).toEqual(3)
+  })
 })
--- client/src/quiz_logic/Quiz.js
+++ client/src/quiz_logic/Quiz.js
@@ -1,8 +1,16 @@
 class Quiz {
   constructor({dataSet, user}) {
     this.dataSet = dataSet
-    this.user = user 
+    this.user = user
+    this.round = 1 
+    this.cardsOnShow = []
   }
+
+  displayCards() {
+    const numberOfCards = this.round + 1
+    this.cardsOnShow = this.dataSet.slice(0, numberOfCards)
+  }
+
 }
 
 export default Quiz
