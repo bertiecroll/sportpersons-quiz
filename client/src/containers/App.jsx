c2f84c4aba75aa007eec20f38fb982336abfb81d
Bertie Croll <bert.croll@gmail.com>
2017-02-03 19:11:26 +0000

    componentDidMount method added to App

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
+import Quiz from '../quiz_logic/Quiz'
+import Collection from '../quiz_logic/Collection'
+import Tracker from '../quiz_logic/Tracker'
+import WinChecker from '../quiz_logic/WinChecker'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      quiz: null
+    }
+    this.apiResponseAction = this.apiResponseAction.bind(this)
+  }
+
+  componentDidMount() {
+    const request = new XMLHttpRequest()
+    request.open('GET', this.props.url)
+    request.onload = this.apiResponseAction
+    request.send(null)
   }
 
   render() {
+    console.log(this.state.quiz)
     return (
       <div className="app-container">
         Sportpersons Quiz
       </div>
     )
+  }
+
+  apiResponseAction(event) {
+    const jsonString = event.target.responseText
+    const data = JSON.parse(jsonString)
+    const quiz = new Quiz({
+      collection: new Collection(data.sportspeople),
+      winChecker: new WinChecker(),
+      tracker: new Tracker({totalRounds: 4}),
+      user: {name: "Player 1"},
+      compareBy: 'points'
+    })
+    this.setState({
+      quiz: quiz
+    })
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
 
+const url = "https://gist.githubusercontent.com/joshheald/d26b89b0fbaf4e26cb423913ada21b83/raw/174a7ac0916919d4ae171adcfc0af78811b185f3/sportspeople.json"
+
 window.onload = function() {
   ReactDOM.render(
-    <App />,
+    <App url={url}/>,
     document.getElementById('app')
   )
 }
