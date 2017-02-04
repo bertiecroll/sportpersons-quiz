777bcf1d280ee3df55a7ca8bd723c9680ad4c63a
Bertie Croll <bert.croll@gmail.com>
2017-02-04 18:28:35 +0000

    quiz actiontypes created

---
+++ client/src/actiontypes/quiz.js
@@ -0,0 +1,4 @@
+export const SET_QUIZ_DATA = "quiz/SET_QUIZ_DATA"
+export const SET_USER = "quiz/SET_USER"
+export const UPDATE_CARDS_ON_SHOW = "quiz/UPDATE_CARDS_ON_SHOW"
+export const UPDATE_SCORE_CARD = "quiz/UPDATE_SCORE_CARD"
