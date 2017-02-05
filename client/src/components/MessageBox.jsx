aa20f5cc51d72d3aa80ae3b2f6f07ae81b1e7844
Bertie Croll <bert.croll@gmail.com>
2017-02-05 19:40:18 +0000

    styling added to Display component

--- client/src/components/MessageBox.jsx
+++ client/src/components/MessageBox.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const MessageBox = function({user, chosenCard, round}) {
 
   const content = (chosenCard) ?
-  <h4>Your Selection: {chosenCard.full_name}</h4> :
-  <h4> </h4>
+  <h3 className="selected-choice">Your Selection: <strong>{chosenCard.full_name}</strong></h3> :
+  <h3> </h3>
 
   return (
     <div className="message-box">
-      <h2>Player: {user}</h2>
-      <h2>Round: {round}</h2>
+      <h2 className="user-name">Player: <strong>{user}</strong></h2>
+      <h2>Round: <strong>{round}</strong></h2>
       {content}
     </div>
   )
