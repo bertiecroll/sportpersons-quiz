1cdfcf12b78f9fe7c7843db3bee94e2e19dbc374
Bertie Croll <bert.croll@gmail.com>
2017-02-06 09:41:45 +0000

    bug fix in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import path from 'path'
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static('client/build'))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(`${__dirname}/client/build/index.html`))
